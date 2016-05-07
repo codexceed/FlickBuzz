ce6957ca5b8eb6e2964d54a2fd7d4f0e884f327d
codexceed <sarthak.joshi94@yahoo.in>
2016-05-08 00:24:42 +0530

    Tile Formatting
    Formatted movie posters into gridview tiles. Added API url

--- app/src/main/java/com/example/android/flickbuzz/MovieFragment.java
+++ app/src/main/java/com/example/android/flickbuzz/MovieFragment.java
@@ -13,6 +13,8 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by XCEED on 03-05-2016.
  */
@@ -30,9 +32,68 @@ public class MovieFragment extends Fragment {
         View rootview = inflater.inflate(R.layout.movie_grid, container, false);
 
         GridView gridview = (GridView) rootview.findViewById(R.id.movie_grid_view);
-
+        gridview.setAdapter(new ImageAdapter(getActivity()));
 
         return rootview;
     }
 
+
+    public class ImageAdapter extends BaseAdapter {
+        public Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            return 9;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if(convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                imageView.setPadding(0, 0, 0, 0);
+                imageView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.MATCH_PARENT, (int) mContext.getResources().getInteger(R.integer.poster_height)));
+            }
+            else {
+                imageView = (ImageView) convertView;
+            }
+
+            Picasso.with(mContext).load("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg")
+                    .into(imageView);
+            return imageView;
+        }
+        private Integer[] mThumbIds = {
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7
+        };
+
+
+    }
+
 }
+
+
